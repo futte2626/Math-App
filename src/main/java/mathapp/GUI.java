f6a8b79d098efd20f75e1006681d5190f1a9d0b9
futte2626 <carl-emil@fambjornbak.dk>
2025-10-02 17:47:12 +0200

    Added a screen

--- src/main/java/mathapp/GUI.java
+++ src/main/java/mathapp/GUI.java
@@ -1,4 +1,20 @@
 package mathapp;
 
+import javax.swing.*;
+
 public class GUI {
+    public void GUI() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Math App");
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+    }
+
+
 }
--- src/main/java/mathapp/Main.java
+++ src/main/java/mathapp/Main.java
@@ -2,6 +2,8 @@ package mathapp;
 
 public class Main {
     public static void main(String[] args) {
+        GUI gui = new GUI();
+
         System.out.println("Hello World");
     }
 }
